15f38768b30f516209c72304df18d4210c2ed192
Mario Lazzari <mario.lazzari@gmail.com>
2018-08-02 16:32:02 +0200

    Done async / await examples

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -6,14 +6,33 @@
 
 const axios = require("axios");
 
-const getExchangeRate = (from, to) => {
-    const url = `http://data.fixer.io/api/latest?access_key=266599b236d6bfa752a6ff8b75c56051&base=${from}`;
-    return axios.get(url).then(res => res.data.rates[to]);
+const getExchangeRate = async (from, to) => {
+    try {
+        const url = `http://data.fixer.io/api/latest?access_key=266599b236d6bfa752a6ff8b75c56051&base=${from}`;
+        const response = await axios.get(url);
+        const rate = response.data.rates[to];
+
+        if (rate) {
+            return rates;
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        throw new Error(`Unable to find a rate from ${from} to ${to}`);
+    }
 };
 
-const getCountries = currencyCode => {
-    const url = `https://restcountries.eu/rest/v2/currency/${currencyCode}`;
-    return axios.get(url).then(res => res.data.map(country => country.name));
+const getCountries = async currencyCode => {
+    try {
+        const url = `https://restcountries.eu/rest/v2/currency/${currencyCode}`;
+        const response = await axios.get(url);
+
+        return response.data.map(country => country.name);
+    } catch (error) {
+        throw new Error(
+            `Unable to fetch country that use currency code ${currencyCode}`
+        );
+    }
 };
 
 /*
@@ -30,6 +49,7 @@ getCountries("EUR")
 const convertCurrency = async (from, to, value) => {
     const rate = await getExchangeRate(from, to);
     const countries = await getCountries(to);
+
     const conversion = rate * value;
 
     return countries.map(
@@ -37,6 +57,6 @@ const convertCurrency = async (from, to, value) => {
     );
 };
 
-convertCurrency("EUR", "USD", 100)
+convertCurrency("EUR", "yyy", 100)
     .then(status => console.log(status))
-    .catch(err => console.log("ERRORE", err));
+    .catch(err => console.log(err.message));
